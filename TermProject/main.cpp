913060a7285b70cbe0bb1f65b01291acb07b951e
qawbecrdtey <qawbecrdtey@naver.com>
2019-10-29 13:42:42 +0900

    Added Term project

---
+++ TermProject/main.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    std::string input_file_name = "input.txt";
+    std::string source;
+    // The first element denotes the input file.
+    // If no first element exists, then the default is "input.txt".
+    if(argc < 2) {
+        std::cout << "Using input file input.txt as default." << std::endl;
+    }
+    else {
+        std::cout << "Using input file " << (input_file_name = argv[1]) << '.' << std::endl;
+    }
+
+    std::ifstream in(input_file_name);
+    // If there is no input file with such name, exit the program.
+    if(!in) {
+        std::cerr << "Couldn't find file " << input_file_name << "!" << std::endl;
+        return 1;
+    }
+    // Saves the input into source.
+    in.seekg(0, std::ios::end);
+    source.resize(in.tellg());
+    in.seekg(0, std::ios::beg);
+    in.read(&source[0], source.size());
+    in.close();
+
+    
+}
