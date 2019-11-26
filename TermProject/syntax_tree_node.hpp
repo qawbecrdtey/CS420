21bc6b278472b7f7a39bec979c2ba018aad80a79
qawbecrdtey <qawbecrdtey@naver.com>
2019-11-26 17:17:04 +0900

    Modified syntax_tree_node.hpp

--- TermProject/syntax_tree_node.hpp
+++ TermProject/syntax_tree_node.hpp
@@ -12,18 +12,26 @@ struct syntax_tree_node {
 	std::vector<std::unique_ptr<syntax_tree_node>> children;
 
 	explicit syntax_tree_node(std::int64_t size) {
-		if (size > -1) {
-			children.reserve(size);
-		}
+		if (size > -1) children.reserve(size);
 	}
-	syntax_tree_node(syntax_tree_node const &);
-	syntax_tree_node(syntax_tree_node &&) noexcept;
-	syntax_tree_node const &operator=(syntax_tree_node const &);
-	syntax_tree_node const &operator=(syntax_tree_node &&) noexcept;
+	syntax_tree_node(syntax_tree_node const &stn) {
+		children = stn.children;
+	}
+	syntax_tree_node(syntax_tree_node &&stn) noexcept {
+		std::exchange(children, stn.children);
+	}
+	syntax_tree_node const &operator=(syntax_tree_node const &stn) {
+		if (&stn != this) children = stn.children;
+		return *this;
+	}
+	syntax_tree_node const &operator=(syntax_tree_node &&stn) noexcept {
+		if (&stn != this) std::exchange(children, stn.children);
+		return *this;
+	}
 	~syntax_tree_node() = default;
 
 	void add_node(std::unique_ptr<syntax_tree_node> &&node);
-	// virtual void parse(std::vector<std::string> const &) = 0;
+	virtual void parse(std::vector<std::string> const &) = 0;
 };
 
 #endif
