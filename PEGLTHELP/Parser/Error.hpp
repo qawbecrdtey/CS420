47b16c1c0a03ada1b955ef2c3e1f2b304d6b4e9b
HealthyTeeth <shlee259p@kaist.ac.kr>
2019-12-23 11:25:46 +0900

    error.hpp added

--- PEGLTHELP/Parser/Error.hpp
+++ PEGLTHELP/Parser/Error.hpp
@@ -4,8 +4,37 @@
 #include <iostream>
 #include <type_traits>
 
-#include <tao/pegtl/contrib/parse_tree.hpp>
-
 #include "Grammar.hpp"
+
+namespace Parser{
+	using namespace tao::pegtl;
+
+	struct Escaped : seq < one<'\\'>, must < one < 'n', 'r', 't' > > > {};
+	struct Content : tao::pegtl::star < sor< Escaped, not_one< '"' > > > {};
+	struct Literal : seq < one< '"'>, Content, one< '"' > > {};
+
+	template <typename Rule>
+	struct control
+		: tao::pegtl::normal< Rule >
+	{
+		static const std::string error_msg;
+
+		template< typename Input, typename... States >
+		static void raise(const Input& in, States&&...)
+		{
+			throw tao::petgl::parse_error(error_meg, in);
+		}
+	};
+
+	template<> const std::string control<semicolon>::error_msg = "expected ;";
+	template<> const std::string control<digit>::error_msg = "expected digit";
+	template<> const std::string control<closeparen>::error_msg = "expected )";
+	template<> const std::string control<closecurly>::error_msg = "expected }";
+	template<> const std::string control<closebrack>::error_msg = "expected ]";
+	template<> const std::string control<Expression>::error_msg = "expected expression";
+	template<> const std::string control<string_literal>::error_msg = "expected string";
+	template<> const std::string control<comma>::error_msg = "expected ,";
+	template<> const std::string control<Compound_statement>::error_msg = "expected Compound statement";
+}
 
 #endif
